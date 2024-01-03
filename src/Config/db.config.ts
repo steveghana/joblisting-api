616fd2edb9a28c41a21280234986d282f158c06f
steveghana <boatengstephen707@gmail.com>
2024-01-03 11:46:32 +0000

    update host url to database config

--- src/Config/db.config.ts
+++ src/Config/db.config.ts
@@ -8,11 +8,11 @@ export default registerAs('database', () => ({
   type: 'postgres',
   logging: true,
   url: process.env.DB_DATABASE_URL,
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'postgres',
+  // host: process.env.DB_HOST || 'localhost',
+  // port: process.env.DB_PORT || 5432,
+  // username: process.env.DB_USERNAME || 'postgres',
+  // password: process.env.DB_PASSWORD || 'password',
+  // database: process.env.DB_NAME || 'postgres',
 
   autoLoadEntities: true,
   extra: {
